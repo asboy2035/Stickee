ec862d81bc7a6cc303aa9e98eebdb2343b28afeb
ash <asboy2035@icloud.com>
2025-02-05 19:04:00 +0100

    Initial Commit

---
+++ Stickee/Item.swift
@@ -0,0 +1,18 @@
+//
+//  Item.swift
+//  Stickee
+//
+//  Created by ash on 2/5/25.
+//
+
+import Foundation
+import SwiftData
+
+@Model
+final class Item {
+    var timestamp: Date
+    
+    init(timestamp: Date) {
+        self.timestamp = timestamp
+    }
+}
---
+++ StickeeTests/StickeeTests.swift
@@ -0,0 +1,17 @@
+//
+//  StickeeTests.swift
+//  StickeeTests
+//
+//  Created by ash on 2/5/25.
+//
+
+import Testing
+@testable import Stickee
+
+struct StickeeTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
